76ff766cfdbe4885fea1163688e114fde9925b4c
odisho1997 <86355012+odisho1997@users.noreply.github.com>
2021-08-11 21:38:36 +0300

    Update bot_pms.py

--- userbot/assistant/bot_pms.py
+++ userbot/assistant/bot_pms.py
@@ -48,10 +48,10 @@ async def check_bot_started_users(user, event):
     check = get_starter_details(user.id)
     if check is None:
         start_date = str(datetime.now().strftime("%B %d, %Y"))
-        notification = f"**▾∮مرحبا عزيزي ↸**\n**▾ قام المستخدم ↫ ** 『{_format.mentionuser(user.first_name , user.id)}』 **بتشغيل البوت❕**\n\n**▾∮الاسم ⪼** `{get_display_name(user)}`\n**▾∮الايدي ⪼ **`{user.id}`\n[▾∮ՏøuƦcε πøνεʍβεƦ 🌦](https://t.me/nneee)"
+        notification = f"**▾∮مرحبا عزيزي ↸**\n**▾ قام المستخدم ↫ ** 『{_format.mentionuser(user.first_name , user.id)}』 **بتشغيل البوت❕**\n**▾∮الاسم ⪼** `{get_display_name(user)}`\n**▾∮الايدي ⪼ **`{user.id}`\n\n[▾∮ՏøuƦcε πøνεʍβεƦ 🌦](https://t.me/nneee)"
     else:
         start_date = check.date
-        notification = f"**▾∮ قام المستخدم ↫ ** 「{_format.mentionuser(user.first_name , user.id)}」 **بإعادة تشغيل البوت❗️**\n\n**▾∮الاسم ⪼ **`{get_display_name(user)}`\n**▾∮الايدي ⪼ ** `{user.id}`\n[▾∮ՏøuƦcε πøνεʍβεƦ 🌦](https://t.me/nneee)"
+        notification = f"**▾∮ قام المستخدم ↫ ** 「{_format.mentionuser(user.first_name , user.id)}」 **بإعادة تشغيل البوت❗️**\n**▾∮الاسم ⪼ **`{get_display_name(user)}`\n**▾∮الايدي ⪼ ** `{user.id}`\n\n[▾∮ՏøuƦcε πøνεʍβεƦ 🌦](https://t.me/nneee)"
     try:
         add_starter_to_db(user.id, get_display_name(user), start_date, user.username)
     except Exception as e:
@@ -99,7 +99,7 @@ async def bot_start(event):
                 my_mention=my_mention,
             )
         else:
-            start_msg = f"**▾∮ مرحبا عزيزي ↫ **{mention}\n**▾∮انا البوت المساعد لــ ↫ **{my_mention}\n**يمكنك مراسلة صاحب البوت من خلالي**\n[▾∮ՏøuƦcε πøνεʍβεƦ 🌦](https://t.me/nneee)"
+            start_msg = f"**▾∮ مرحبا عزيزي ↫ **{mention}\n**▾∮انا البوت المساعد لــ ↫ **{my_mention}\n**يمكنك مراسلة صاحب البوت من خلالي**\n\n[▾∮ՏøuƦcε πøνεʍβεƦ 🌦](https://t.me/nneee)"
         buttons = [
             (
                 Button.url(
